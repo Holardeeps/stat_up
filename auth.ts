c09b25bc309c9dce571e62269c7eb20952d5900b
Holardeeps <praiseelijah2001@gmail.com>
2025-12-14 23:51:19 +0100

    Add the pitch input and zod validation with schema

--- auth.ts
+++ auth.ts
@@ -43,3 +43,72 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }
   }
 })
+
+// Safed TYPED
+
+// import NextAuth from "next-auth"
+// import GitHub from "next-auth/providers/github"
+// import { client } from "./sanity/lib/client"
+// import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/queries"
+// import { writeClient } from "./sanity/lib/write-client"
+// import type { GitHubProfile } from "next-auth/providers/github"
+
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [GitHub],
+//   callbacks: {
+//     async signIn({ user, profile }) {
+//       if (!profile) return false
+
+//       const {
+//         id,
+//         login,
+//         bio,
+//       } = profile as GitHubProfile
+
+//       const existingUser = await client.fetch(
+//         AUTHOR_BY_GITHUB_ID_QUERY,
+//         { id }
+//       )
+
+//       if (!existingUser) {
+//         await writeClient
+//           .withConfig({ useCdn: false })
+//           .create({
+//             _type: "author",
+//             githubId: id,
+//             name: user.name,
+//             username: login,
+//             email: user.email,
+//             image: user.image,
+//             bio: bio ?? "",
+//           })
+//       }
+
+//       return true
+//     },
+
+//     async jwt({ token, account, profile }) {
+//       if (account && profile) {
+//         const { id } = profile as GitHubProfile
+
+//         const user = await client
+//           .withConfig({ useCdn: false })
+//           .fetch(AUTHOR_BY_GITHUB_ID_QUERY, { id })
+
+//         if (user?._id) {
+//           token.id = user._id
+//         }
+//       }
+
+//       return token
+//     },
+
+//     async session({ session, token }) {
+//       if (token.id) {
+//         session.id = token.id
+//       }
+
+//       return session
+//     },
+//   },
+// })
---
+++ components/StartupForm.tsx
@@ -0,0 +1,139 @@
+"use client"
+
+import { useActionState, useState } from 'react';
+
+import { Input } from "./ui/input"
+import { Textarea } from './ui/textarea';
+import MDEditor from "@uiw/react-md-editor"
+import { Button } from './ui/button';
+import { Send } from 'lucide-react';
+import { Spinner } from './ui/spinner';
+import { formSchema } from '@/lib/validation';
+
+const StartupForm = () => {
+
+    const [errors, setErrors] = useState<Record<string, string>>({});
+    const [pitch, setPitch] = useState("")
+
+    const handleFormSubmit =  async (prevState: any, formData: FormData) => {
+        try {
+            const formValues = {
+                title: formData.get("title") as string,
+                description: formData.get("description") as string,
+                category: formData.get("category") as string,
+                link: formData.get("link") as string,
+                pitch,
+            }
+
+            await formSchema.parseAsync(formValues);
+            console.log(formValues)
+
+            // const result = await createIdea(prevState, formData, pitch) 
+            // console.log(result)
+
+        } catch ( error ) {
+
+        } finally {
+
+        }
+
+    }
+    const [ state, formAction, isPending ] = useActionState(
+        handleFormSubmit, 
+        {
+            error: "",
+            status: "INITIAL"
+        } 
+    );
+
+    
+  return (
+    <form action="" className="startup-form">
+        <div className="">
+            <label htmlFor="title" className="startup-form_label">Title</label>
+            <Input
+                id="title"
+                name="title"
+                className="startup-form_input"
+                required
+                placeholder="Startup Title"
+             />
+            { errors.title && <p className='startup-form_error'>{errors.title}</p>}
+        </div>
+        <div className="">
+            <label htmlFor="description" className="startup-form_label">Description</label>
+            <Textarea
+                id="description"
+                name="description"
+                className="startup-form_textarea"
+                required
+                placeholder="Startup Description"
+             />
+            { errors.description && 
+                <p className='startup-form_error'>{errors.description}</p>
+            }
+        </div>
+        <div className="">
+            <label htmlFor="category" className="startup-form_label">Title</label>
+            <Input
+                id="category"
+                name="category"
+                className="startup-form_input"
+                required
+                placeholder="Startup Category (Tech, Health, Education...)"
+             />
+            { errors.category && <p className='startup-form_error'>{errors.category}</p>}
+        </div>
+        <div className="">
+            <label htmlFor="link" className="startup-form_label">Image URL</label>
+            <Input
+                id="link"
+                name="link"
+                className="startup-form_input"
+                required
+                placeholder="Startup Image URL"
+             />
+            { errors.link && <p className='startup-form_error'>{errors.link}</p>}
+        </div>
+        <div className="" data-color-mode="light" >
+            <label htmlFor="pitch" className="startup-form_label">Pitch</label>
+            <MDEditor
+                value={pitch}
+                onChange={(value) => setPitch(value as string)}
+                id='pitch'
+                preview='edit'
+                height={300}
+                style={{ borderRadius: 20, overflow: "hidden" }}
+                textareaProps={{
+                    placeholder: "Briefly describe your idea and what problem it solves"
+                }}
+                previewOptions={{
+                    disallowedElements: ["style"]
+                }}
+             />
+            { errors.pitch && <p className='startup-form_error'>{errors.pitch}</p>}
+        </div>
+
+        <Button
+            type='submit'
+            className='startup-form_btn hover:bg-[#ee2b69]! cursor-pointer hover:text-black'
+            disabled={isPending}>
+                {isPending ? (
+  <>
+    Submitting...
+    <Spinner className="size-6 ml-2" />
+  </>
+) : (
+  <>
+    Submit your pitch...
+    <Send className="size-6 ml-2" />
+  </>
+)}
+
+                
+            </Button>
+    </form>
+  )
+}
+
+export default StartupForm
