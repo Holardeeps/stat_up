8db24a448102f2a0532cd7b326de1acc5ced2240
Holardeeps <praiseelijah2001@gmail.com>
2025-12-16 11:34:39 +0100

    edit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -21,7 +21,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
   return (
     <>
     <section className="pink_container">
-      <h1 className="heading">Pitch ypur startup, <br /> connect with entrpreneurs</h1>
+      <h1 className="heading">Pitch your startup, <br /> connect with entrpreneurs</h1>
       <p className="sub-heading max-w-3xl!">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
 
       <SearchForm query={query} />
